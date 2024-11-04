da400cb1dd437e2620ee8dd92cdb9daa8e84e48a
anviks <anviks@ttu.ee>
2024-11-04 12:16:23 +0200

    Use a single variable since the destination should always be mirrored

--- scripts/copy_to_server.ps1
+++ scripts/copy_to_server.ps1
@@ -1,4 +1,3 @@
-$SourcePath = '.\translations'
-$DestinationPath = ''
+$Path = 'scripts'
 
-scp -r -P 8022 $SourcePath "localhost@192.168.1.165:/data/data/com.termux/files/home/discordbot/$DestinationPath"
+scp -r -P 8022 "..\$Path" "localhost@192.168.1.165:/data/data/com.termux/files/home/discordbot/$Path"
